4ab74a71b317905bb8b9739c57551ff753607413
doublealogic <cras.ajrobertson@gmail.com>
2020-12-09 03:26:24 -0500

    Added and updated app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, jsonify, redirect
+import pymongo
+import scrape_mars
+
+app = Flask(__name__)
+
+mongo = pymongo(app, uri="mongodb://localhost:27017/mission_to_mars_app")
+
+@app.route("/")
+def index():
+    mars_data = mongo.db.Mission_to_Mars.find_one()
+
+    # Return template and data
+    return render_template("index.html", mars_data=mars_data)
+
+@app.route("/scrape")
+def scrape():
+    # Activates Scrape function which scrapes websites for facts and news about Mars
+    mtm_scrape = scrape_mars.scrape()
+
+    # Updates Mission to Mars database
+    mongo.db.Mission_to_Mars.update({}, mtm_scrape, upsert = True)
+
+    # Takes you back to the home page
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(debug=True)
